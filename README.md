8b161856a17c827c7a678cd47825851a39a50143
Rab Campbell <56073739+LFCRab@users.noreply.github.com>
2023-03-11 18:36:42 +0000

    Test - set of random tests

---
+++ README.md
@@ -0,0 +1,6 @@
+# vite-unit-testing-research
+Doing some research regarding unit testing with vite and figured i could atleast make a repo from it! 
+
+A pretty dull return back to GitHub from me after a long hiatus, just want to do some research in preparation for work, permitting me to see what vitest and vite are capable of. Nothing super interesting most likely, just a bog standard application with a set of frontend unit tests to accompany them.
+
+:shipit: i'm back :shipit:
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App(props: { title: string }) {
+/**
+ * Default entry point.
+ */
+const App: React.FC<{title: string}> = (props: {title: string}) => {
+  const [number, setNumber] = useState(0);
+  
   return (
     <div className="App">
       <p>{props.title}</p>
+      <h1>Test Title for the page</h1>
+      <img width="200" height="200" src="https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/Slave_Knight_Gael_Buffing.jpg" alt="Gael" />
+        <p>The number is: {number}</p>
+      <button onClick={() => setNumber(number + 1)}>+</button>
+      <button onClick={() => setNumber(number - 1)}>-</button>
     </div>
   )
 }
